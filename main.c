3b6d2a55c6f0dd19e4125414fba61272425c2745
DESKTOP-PJ0CJB9\张钰 <15169580563@163.com>
2016-11-03 21:17:48 +0800

    hai,shoudaolemei

---
+++ main.c
@@ -0,0 +1,123 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<style type="text/css">
+h1 {background-color:rgb(100,50,100);letter-spacing:20px;}
+body{color:red;background-image:url(./xue.jpg);background-repeat:no-repeat;
+background-position:20% 20%;
+background-attachment:fixed;}
+span.highlight{background-color:blue;}
+</style>
+</head>
+
+
+<h1>人生就如同这雨水 </h1>
+<body>
+<p>hi，朋友</p>
+ <p><span class="highlight">人生如同这雨水，自天而生，</span>落地而陨。而这从天到地的过程，便是人生。</p>
+ <a href="http://www.baidu.cn">点开看看</a>
+ <p>额，谁说大学很轻松的，拉出去斩了。</br>哎，既不想宅在宿舍，与不能在图书馆安心读书，</br>神魔鬼，怀疑人生</p>
+ </br>
+ <address>
+
+ written by<a href="http://m.sogou.com">再点一下</a>.</address>
+ 
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+ 
+
+ <blokequote>
+ qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq</blokequote>
+ <table border="1">
+ <tr>
+ <td>姓名 学号 性别</td>
+ </tr>
+ <tr>
+ <td>李凯 1607040115 男</td>
+ </tr>
+
+ </body>
+ </html>
+<!DOCTYPE html>
+<html>
+<head>
+<style type="text/css">
+h1 {background-color:rgb(100,50,100);letter-spacing:20px;}
+body{color:red;background-image:url(./xue.jpg);background-repeat:no-repeat;
+background-position:20% 20%;
+background-attachment:fixed;}
+span.highlight{background-color:blue;}
+</style>
+</head>
+
+
+<h1>人生就如同这雨水 </h1>
+<body>
+<p>hi，朋友</p>
+ <p><span class="highlight">人生如同这雨水，自天而生，</span>落地而陨。而这从天到地的过程，便是人生。</p>
+ <a href="http://www.baidu.cn">点开看看</a>
+ <p>额，谁说大学很轻松的，拉出去斩了。</br>哎，既不想宅在宿舍，与不能在图书馆安心读书，</br>神魔鬼，怀疑人生</p>
+ </br>
+ <address>
+
+ written by<a href="http://m.sogou.com">再点一下</a>.</address>
+ <p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+<p>图像不会随页面的其余部分滚动。</p>
+ 
+
+ <blokequote>
+ qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq</blokequote>
+ <table border="1">
+ <tr>
+ <td>姓名 学号 性别</td>
+ </tr>
+ <tr>
+ <td>李凯 1607040115 男</td>
+ </tr>
+
+ </body>
+ </html>
+
